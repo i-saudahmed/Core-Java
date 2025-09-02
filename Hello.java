a0944bfd41a531f7844b986291aa3f79307920c0
i-saudahmed <ahmedsaud00785@gmail.com>
2025-09-02 17:45:53 +0500

    learning

--- Hello.java
+++ Hello.java
@@ -1,3 +1,37 @@
-public class Hello {
-    
+class Hello {
+    public static void main(String args[]) {
+        // int num = 4;
+        // byte num1 = 3;
+        // long l = 34343434l;
+        // short num4 = 1;
+
+        // float marks = 23.31f;
+        // double mark1 = 34.23243;
+        // int num333 = 23_000_99;
+
+        // char c = 'a'; // here a is the literal
+        // c++;
+        // System.out.println(c);
+        // boolean b = true;
+        // int a = 256;
+        // byte b = 127;
+        // // b = (byte) a;
+        // a = b;
+        // System.out.println(a);
+
+        // float f = 23.4f;
+        // int x = (int) f;
+        // System.out.println(f);
+        int num = 343;
+        String s = String.valueOf(num);
+
+        System.out.println(s);
+
+        // System.out.println(((Object) a).getClass().getName());
+
+        // System.out.println(num333);
+
+        // System.out.println("I love Java");
+        // System.out.println(((Object) num).getClass().getName());
+    }
 }
