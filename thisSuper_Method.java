07121c7abe4fd107d71abe6a3ec5de89639b30b4
i-saudahmed <ahmedsaud00785@gmail.com>
2025-09-07 10:43:47 +0500

    changes in files

---
+++ thisSuper_Method.java
@@ -0,0 +1,28 @@
+class A extends Object {
+    public A() {
+        System.out.println("in A ");
+    }
+
+    public A(int n) {
+        System.out.println("in A  value");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("in B ");
+    }
+
+    public B(int n) {
+        this();
+        // super(n);
+        System.out.println("in B value");
+    }
+}
+
+public class thisSuper_Method {
+    public static void main(String[] args) {
+        B obj = new B(5);
+    }
+}
